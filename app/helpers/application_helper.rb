89d4cefa06fcea714ac62657c34506d5435ea5c7
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-17 23:33:47 +0900

    Initialize repository

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title)
+    	base_title = "Sil-地域子育て情報コミュニティ"
+    	if page_title.empty?
+      		base_title
+    	else
+      		"#{base_title} | #{page_title}"
+    	end
+  	end
 end
