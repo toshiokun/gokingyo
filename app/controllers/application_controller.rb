f302edeb78d03df2d60cd41a18dfbad97b700a7b
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-18 23:55:14 +0900

    イベント参加機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :name
+      	devise_parameter_sanitizer.for(:sign_up) do |u|
+      		u.permit(:name, :profile,
+        		:email, :password, :password_confirmation)
+    	end
+    	devise_parameter_sanitizer.for(:account_update) do |u|
+     	 	u.permit(:name, :profile,
+        		:email, :password, :password_confirmation, :current_password)
+    	end
     end
 end
