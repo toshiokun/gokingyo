47f1a2ab1bad60b04c0e5d7e4f5738bdb185797f
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-20 00:07:53 +0900

    seed作成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Sil::Application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'ec2-107-21-114-132.compute-1.amazonaws.com', port: 5432 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "sil.register.mail@gmail.com",
+    :password => "lionsapart2248",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
