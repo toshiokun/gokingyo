2c5aa76f5240114781c30ad4d5616d8f4ab481d8
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-20 00:13:44 +0900

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
 
-user = User.new(email: 'gokingyo@sil.com', password: 'gokingyo', name: "gokingyo")
-user.save!
+user = User.new(email: 'gokingyo@sil.com', password: 'gokingyo', password_confirmation: 'gokingyo', name: "ご金魚")
+user.save
+user.confirm!
