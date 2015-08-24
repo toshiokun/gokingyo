f048787d2a4d14577e7c44a104899e9096eb31f1
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-08-25 00:28:57 +0900

    プロフィール機能実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def index
+    count = 0
+    current_user.microposts.each do |m|
+      count = count + m.favorites.count
+    end
+    @count = count
   end
 
   def show
